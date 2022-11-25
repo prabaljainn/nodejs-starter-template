4c413837e736e1203021bc89eae923099add93da
Prabal Jain <prabaljainn@gmail.com>
2022-11-25 18:07:13 +0530

    milestone 3

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const userRouter = require("./routes/user.js");
+const orderRouter = require("./routes/order.js");
 const morgan = require("morgan");
 
 const app = express();
@@ -7,5 +8,6 @@ const app = express();
 app.use(morgan("dev"));
 
 app.use("/v1/user", userRouter);
+app.use("/v1/order", orderRouter);
 
 module.exports = app;
