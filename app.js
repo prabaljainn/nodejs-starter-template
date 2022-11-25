2f3b4e458f29c1fbf39d7ae737dfeb30ba8d6114
Prabal Jain <prabaljainn@gmail.com>
2022-11-25 18:01:30 +0530

    code updated

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const userRouter = require("./routes/user.js");
+const morgan = require("morgan");
 
 const app = express();
+
+app.use(morgan("dev"));
+
+app.use("/v1/user", userRouter);
 
 module.exports = app;
---
+++ controllers/user.js
@@ -0,0 +1,3 @@
+exports.addUser = async (req, res, next) => {
+  res.status(200).json({ data: "add-user from controller" });
+};
